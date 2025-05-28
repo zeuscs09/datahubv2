e92189801a55f3f90db981b89f0ae8260c14063a
tonjingjai <nattapong@jingjaisoft.com>
2025-05-28 17:11:49 +0700

    ปรับปรุง: แก้ไขการเข้าถึงค่าจากฟังก์ชัน get_lookup_formula ในฟังก์ชัน update_sd_lookup เพื่อให้ได้ค่าที่ถูกต้องสำหรับ gg_milk_currently_consuming

--- datahubv2/core/lookup.py
+++ datahubv2/core/lookup.py
@@ -66,7 +66,7 @@ def update_sd_lookup(main_profile, child_profile):
             lastpro_str = str(child_profile.lastpro) if child_profile.lastpro is not None else ""
             lastformula_str = str(child_profile.lastformula) if child_profile.lastformula is not None else ""
             lastpro_key = lastpro_str + "-" + lastformula_str
-            child_profile.gg_milk_currently_consuming = get_lookup_formula("IC360", lastpro_key)
+            child_profile.gg_milk_currently_consuming = get_lookup_formula("IC360", lastpro_key).value
         
         child_profile.save(ignore_permissions=True)
     
